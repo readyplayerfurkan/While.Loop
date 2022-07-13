c7f3dd9f4f4444e4a0f9f4f59b159c3bffbd9041
fullstackfurkan <furkanyilmaz870@gmail.com>
2022-07-13 09:41:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+// kullanıcıdan bir sayı al ve o sayının ortalamasını kullanıcıya döndür.
+
+Console.Write("Lütfen bir sayı giriniz : ");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 0;
+int toplam = 0;
+
+while (sayac <= sayi)
+{
+    toplam += sayac;
+    sayac++;
+}
+
+Console.WriteLine("Sayıların ortalaması : " + toplam / sayi);
+
+// a'dan z'ye kadar olan tüm harfleri konsola yazdır.,
+
+char character = 'a';
+
+while (character <= 'z')
+{
+    Console.Write(character);
+    character++;
+}
+
+//foreach
+
+string[] diller = { "C#", "Java", "JavaScript", "Phyton" };
+
+foreach (var dil in diller)
+{
+    Console.WriteLine(dil);
+}
